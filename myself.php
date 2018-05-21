b5db3fe37642715652d94a7266feafac7c4dcb04
poozers <wilde.craig@hotmail.com>
2018-05-21 20:44:09 +0100

    Add files via upload

---
+++ myself.php
@@ -0,0 +1,32 @@
+<?php
+
+$first_name = "craig";
+$last_name = "wilde";
+$age = 35;
+$day = 27;
+$Month = "October";
+$year = 1982;
+echo "My name is " .$first_name ." ".$last_name . " my age is " .$age ;
+
+echo " ,I was born on " .$day ."th ".$Month ." ".$year;
+?>
+
+<?php
+
+$Music = array("Guitar","Bass","drums" );
+
+echo "Craig can play " . $Music[0] . " and " . $Music[1] . " and " . $Music[2] . ".";
+
+?>
+<?php
+$job = "PO";
+
+if ($job == "PiO") {
+echo "Craigs job is " .$job ;
+} else {
+  print "Craig has no job";
+}
+
+
+
+?>
